f65f5e375f6e685114abfcb8026c92d7c5fb7252
GoracioNewport <bdfyljkub@gmail.com>
2023-04-12 15:36:23 +0300

    Add Countdown teleportation

--- src/main/java/mcup/core/stages/Countdown.java
+++ src/main/java/mcup/core/stages/Countdown.java
@@ -1,6 +1,7 @@
 package mcup.core.stages;
 
 import mcup.core.Core;
+import mcup.core.local.data.Team;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.title.Title;
 import org.bukkit.*;
@@ -8,11 +9,11 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 
 public class Countdown extends GamemodeStage {
-
-
   @Override
   public void tick() {
     super.tick();
@@ -105,10 +106,64 @@ public class Countdown extends GamemodeStage {
     core.apiManager.playerManager.setGlobalGamemode(GameMode.ADVENTURE);
   }
 
-
   @Override
   public String getDisplayName() {
     return "Обратный отсчёт";
+  }
+
+  protected int spawnOffset = 1;
+  public void buildCage(Material fill) {
+    for (Location location : spawnLocations) {
+      Location anchor = location.clone().add(0, 1, 0);
+
+      int radius = spawnOffset + 1;
+
+      for (int i = -radius - 1; i <= radius + 1; i++) {
+        anchor.clone().add(i, 0, -radius - 1).getBlock().setType(fill);
+        anchor.clone().add(i, 0, radius + 1).getBlock().setType(fill);
+        anchor.clone().add(-radius - 1, 0, i).getBlock().setType(fill);
+        anchor.clone().add(radius + 1, 0, i).getBlock().setType(fill);
+      }
+    }
+  }
+
+  public void spawnPlayers() {
+
+    int locationIndex = 0;
+    for (Team team : core.apiManager.teamManager.getTeams().values()) {
+
+      Location teamLocation = spawnLocations.get(locationIndex).clone();
+      ArrayList< mcup.core.local.data.Player> shuffledPlayers = (ArrayList<mcup.core.local.data.Player>) team.players.clone();
+      Collections.shuffle(shuffledPlayers);
+
+
+      for (int i = 0; i < team.players.size(); i++) {
+        int offsetX = (int)Math.pow(-1, i / 2) * spawnOffset;
+        int offsetZ = (int)Math.pow(-1, i % 2) * spawnOffset;
+
+        Location playerLocation = teamLocation.clone().add(offsetX, 0, offsetZ);
+
+        mcup.core.local.data.Player teamPlayer = shuffledPlayers.get(i);
+        core.apiManager.playerManager.playerTeleport(playerLocation, teamPlayer.nickname);
+      }
+
+      locationIndex++;
+    }
+
+  }
+
+  public ArrayList <Location> spawnLocations = new ArrayList<>();
+
+  protected void getSpawnLocations() {
+    spawnLocations.clear();
+
+    for (int i = 0; i < core.apiManager.teamManager.getTeams().size(); i++) {
+      Location location = plugin.getConfig().getLocation("spawn." + i + ".location");
+//      int offset = plugin.getConfig().getInt("spawn." + i + ".offset");
+      spawnLocations.add(location);
+    }
+
+    Collections.shuffle(spawnLocations);
   }
 
   public Countdown(Core core_, JavaPlugin plugin_) {
